ee86bf77c01e4fc5924715da925ad93b27b0c95b
Vishal Yadav <63285472+vishalYadav8@users.noreply.github.com>
2020-11-17 01:14:30 +0530

    My website

---
+++ CSS-mysite/js/CV.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $("button").click(function(){
+      $("#test").hide();
+    });
+  });
+
+linkedin=function()
+{
+    window.location.href= "https://www.linkedin.com/in/vishalyadav8/";
+}
